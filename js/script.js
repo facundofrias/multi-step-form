e8f4835da6ffe55b0a3432ceb110c215d58edd97
Facundo Frias <facu.fr87@gmail.com>
2023-01-21 20:49:43 -0300

    Se completó el diseño mobile. Se comenzó con la lógia js.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let tab = 0;
+mostrarPestaña(tab);
+
+// Funciones
+function mostrarPestaña(tab) {
+  let tabs = document.getElementsByClassName("tab");
+  switch (tab) {
+    case 0:
+      tabs[tab].style.display = "inline";
+      break;
+    case 1:
+      tabs[tab].style.display = "inline";
+      break;
+    case 2:
+      tabs[tab].style.display = "inline";
+      break;
+    case 3:
+      tabs[tab].style.display = "inline";
+      break;
+  }
+}
